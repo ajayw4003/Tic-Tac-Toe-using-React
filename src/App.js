155da899bb4e66ef3af16b0f1789a2b7a01a33c0
ajayw4003 <ajayw4003@gmail.com>
2021-02-25 16:58:04 +0530

    changed className

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Game from "./components/Game/Game";
 function App() {
   return (
     <div className="App">
-      <h1 className = "title">Tic Tac Toe Game Using React</h1>
+      <h1 className = "name">Tic Tac Toe Game Using React</h1>
       <Game />
     </div>
   );
